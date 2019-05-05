57f7ecd4afa1d3fc22be95d619a01201b6bf92a1
Joel K <moonsky989@users.noreply.github.com>
2019-05-05 12:08:01 -0500

    Update tb_balance_manager.vhd

--- tb_balance_manager.vhd
+++ tb_balance_manager.vhd
@@ -13,7 +13,7 @@ architecture test of tb_balance_manager is
 	signal num_dollars, num_quarters, num_dimes, num_nickles : unsigned(3 downto 0);
 	signal new_currency_interrupt : std_logic;
 	signal order_cost : ufixed(4 downto -5);
-	signal vend, return_balance, reset, clk : std_logic;
+	signal dispensed, return_balance, reset, clk : std_logic;
 	signal insufficient_funds, return_currency_interrupt : std_logic; 	
 	signal return_dollars, return_quarters, return_dimes, return_nickles : unsigned(3 downto 0); 
 	constant t_c : time := 50 ns;
@@ -22,7 +22,7 @@ begin
 	-- instantiate DUV
 	duv_balance_manager: entity work.balance_manager(behavioral)
 		port map (num_dollars=>num_dollars, num_quarters=>num_quarters, num_dimes=>num_dimes, num_nickles=>num_nickles,
-			  new_currency_interrupt=>new_currency_interrupt, order_cost=>order_cost, vend=>vend,
+			  new_currency_interrupt=>new_currency_interrupt, order_cost=>order_cost, dispensed=>dispensed,
 			  return_balance=>return_balance, reset=>reset, clk=>clk, insufficient_funds=>insufficient_funds, 
 			  return_currency_interrupt=>return_currency_interrupt,
 			  return_dollars=>return_dollars, return_quarters=>return_quarters, return_dimes=>return_dimes, return_nickles=>return_nickles);
@@ -48,19 +48,19 @@ begin
 		num_dimes <= to_unsigned(num_dimes_in,num_dimes'length);
 		num_nickles <= to_unsigned(num_nickles_in,num_nickles'length);
 		new_currency_interrupt <= '1';
-		vend <= '1';
+		dispensed <= '1';
 		return_balance <= '1';
 		for i in 1 to 7 loop
 		wait until rising_edge(clk);
 		new_currency_interrupt <= '0';
 		end loop;
-		vend <= '0';
+		dispensed <= '0';
 		return_balance <= '0';
 	end procedure apply_test;
 
 	begin
 	return_balance <= '0';
-	vend <= '0';
+	dispensed <= '0';
         wait until rising_edge(clk) and reset = '0';
 	order_cost <= to_ufixed(1.00, order_cost); -- Order $1.00
 	apply_test(1,2,1,1); -- Load $1.65
