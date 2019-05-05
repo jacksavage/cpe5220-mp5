ee9388c4074633f35fd06d1188888555a448d9d3
Joel K <moonsky989@users.noreply.github.com>
2019-05-05 12:07:45 -0500

    Update balance_manager.vhd

--- balance_manager.vhd
+++ balance_manager.vhd
@@ -14,7 +14,7 @@ entity balance_manager is
 	port( num_dollars, num_quarters, num_dimes, num_nickles : in unsigned(3 downto 0); -- Max 16 of each type, totals $22.40
 	      new_currency_interrupt : in std_logic; -- interrupt when dollar/coin manager gets new currency
 	      order_cost : in ufixed(4 downto -5); -- $32.00 with 0.03125 resolution, max order cost < $22.40
-	      vend, return_balance, reset, clk : in std_logic; -- vend and return balance commands from state machine, vend subtracts order cost from balance, and return_balance returns money to dollar/coin manager
+	      dispensed, return_balance, reset, clk : in std_logic; -- vend and return balance commands from state machine, vend subtracts order cost from balance, and return_balance returns money to dollar/coin manager
 	      insufficient_funds, return_currency_interrupt : out std_logic; -- insufficient_funds signal to state machine, return_currency_interrupt to dollar/coin manager when return_balance outputs are ready
 	      return_dollars, return_quarters, return_dimes, return_nickles : inout unsigned(3 downto 0)); -- return balance quantity to dollar/coin manager
 end entity balance_manager;
@@ -23,7 +23,6 @@ architecture behavioral of balance_manager is
 
 signal previous_balance, loaded_balance, vended_balance, remaining_balance : ufixed(4 downto -5);
 signal temp_balance1, temp_balance2, temp_balance3, remainder1, remainder2, remainder3, dollars,quarters,dimes,nickles : ufixed(4 downto -5);
-signal vended : integer := 0;
 
 begin
 
@@ -40,14 +39,13 @@ begin
 	end if;
 	end process;
 	
-	calculate_balance: process(clk) is -- when state machine sends vend command, calculate remaining_balance or insufficient_funds
+	calculate_balance: process(order_cost) is -- when state machine sends vend command, calculate remaining_balance or insufficient_funds
 	begin
-	if (vend = '1' and reset = '0') then
+	if (reset = '0') then
 		if (loaded_balance > order_cost) then
 			vended_balance <= resize(loaded_balance-order_cost, remaining_balance);
 			previous_balance <= vended_balance; -- in case user loads more money
 			insufficient_funds <= '0';
-			vended <= 1;
 		elsif (loaded_balance < order_cost) then
 			insufficient_funds <= '1';
 		end if;
@@ -57,7 +55,7 @@ begin
 	return_currency: process(clk) is -- return remaining_balance if vend action occurs or loaded_balance if order is canceled
 	begin
 	
-	if (vended=1) then
+	if (dispensed='1') then
 		remaining_balance <= vended_balance;
 	else
 		remaining_balance <= loaded_balance;
